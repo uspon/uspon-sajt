0186014004c529e68d01d3294bdba758c683f4d2
Dušan Simić <dusan.simic1810@gmail.com>
2018-02-02 09:46:05 +0100

    Izmena u dizajnu nav bara
    Svi linkovi za stranice u nav baru u desktop verziji su pomerini sa leva
    na desno.

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
       <b-navbar-toggle target="nav_collapse" id="nav_bar_toggle"></b-navbar-toggle>
       <b-navbar-brand to="/" class="brand"><img src="./assets/uspon-logo-latin.png" alt="USPON" /></b-navbar-brand>
       <b-collapse is-nav id="nav_collapse">
-        <b-navbar-nav>
+        <b-navbar-nav class="ml-auto">
           <b-nav-item to="/" exact>Početna</b-nav-item>
           <b-nav-item to="/contact" exact>Kontakt</b-nav-item>
         </b-navbar-nav>
